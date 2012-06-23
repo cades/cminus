04c36c21ba4fb9abfc4876a798c2faa63ff71d9e
hong-jen kao <endless-dreaming@hotmail.com>
2012-06-23 22:00:25 +0800

    修改 visitor 架構 visitor 子類別不感興趣的 node 不提供空實作，使用父類別的實作。 故空實作全部上移。

--- AST/Visitor/DeclarationProcessing/SemanticsVisitor.h
+++ AST/Visitor/DeclarationProcessing/SemanticsVisitor.h
@@ -14,6 +14,48 @@ class SemanticsVisitor: public NodeVisitor {
 public:
 	SemanticsVisitor();
 	virtual ~SemanticsVisitor();
+
+	virtual void visit(ANDNode& node) {}
+	virtual void visit(ArrayDefiningNode&) {}
+	virtual void visit(ArrayReferencingNode& node) {}
+	virtual void visit(ArrayVariableDeclaringNode& node) {}
+	virtual void visit(AssigningNode& node) {}
+	virtual void visit(BlockNode& node) {}
+	virtual void visit(CallingNode& node) {}
+	virtual void visit(DivideNode& node) {}
+	virtual void visit(EQNode& node) {}
+	virtual void visit(EmptyNode& node) {}
+	virtual void visit(FloatLiteral& node) {}
+	virtual void visit(ForLoopingNode& node) {}
+	virtual void visit(FunctionDeclaringNode& node) {}
+	virtual void visit(GENode& node) {}
+	virtual void visit(GTNode& node) {}
+	virtual void visit(Identifier&) {}
+	virtual void visit(IdentifierWithDim& node) {}
+	virtual void visit(IfTestingNode& node) {}
+	virtual void visit(IntLiteral& node) {}
+	virtual void visit(LENode& node) {}
+	virtual void visit(LTNode& node) {}
+	virtual void visit(MinusNode& node) {}
+	virtual void visit(MultiplyNode& node) {}
+	virtual void visit(NENode& node) {}
+	virtual void visit(NegNode& node) {}
+	virtual void visit(NodeList& node) {}
+	virtual void visit(NotNode& node) {}
+	virtual void visit(ORNode& node) {}
+	virtual void visit(PlusNode& node) {}
+	virtual void visit(ReturningNode& node) {}
+	virtual void visit(StructDefiningNode&) {}
+	virtual void visit(StringLiteral& node) {}
+	virtual void visit(StructReferencingNode& node) {}
+	virtual void visit(TypeDeclaringNode& node) {}
+	virtual void visit(TypeSpecifier& node) {}
+	virtual void visit(TypeSpecifierByName& node) {}
+	virtual void visit(TypeSpecifierByStruct& node) {}
+	virtual void visit(TypeSpecifierWithTypeDecl& node) {}
+	virtual void visit(TypedefNode& node) {}
+	virtual void visit(VariableListDeclaringNode& node) {}
+	virtual void visit(WhileLoopingNode& node) {}
 };
 
 #endif /* SEMANTICSVISITOR_H_ */
--- AST/Visitor/DeclarationProcessing/TopDeclVisitor.h
+++ AST/Visitor/DeclarationProcessing/TopDeclVisitor.h
@@ -19,49 +19,12 @@ public:
 	TopDeclVisitor(SymbolTable*, ostream&);
 	virtual ~TopDeclVisitor();
 
-
 //	virtual void visit(ArrayVariableDeclaringNode& node);
 	virtual void visit(FunctionDeclaringNode& node);
 	virtual void visit(TypeDeclaringNode&);
 	virtual void visit(TypedefNode& node);
 	virtual void visit(VariableListDeclaringNode& node);
 	virtual void visit(NodeList& node) { NodeVisitor::visitChildren(node); }
-
-	virtual void visit(ANDNode& node) {}
-	virtual void visit(ArrayDefiningNode& node) {}
-	virtual void visit(ArrayReferencingNode& node) {}
-	virtual void visit(AssigningNode& node) {}
-	virtual void visit(BlockNode& node) {}
-	virtual void visit(CallingNode& node) {}
-	virtual void visit(DivideNode& node) {}
-	virtual void visit(EQNode& node) {}
-	virtual void visit(EmptyNode& node) {}
-	virtual void visit(FloatLiteral& node) {}
-	virtual void visit(ForLoopingNode& node) {}
-	virtual void visit(GENode& node) {}
-	virtual void visit(GTNode& node) {}
-	virtual void visit(Identifier& node) {}
-	virtual void visit(IdentifierWithDim& node) {}
-	virtual void visit(IfTestingNode& node) {}
-	virtual void visit(IntLiteral& node) {}
-	virtual void visit(LENode& node) {}
-	virtual void visit(LTNode& node) {}
-	virtual void visit(MinusNode& node) {}
-	virtual void visit(MultiplyNode& node) {}
-	virtual void visit(NENode& node) {}
-	virtual void visit(NegNode& node) {}
-	virtual void visit(NotNode& node) {}
-	virtual void visit(ORNode& node) {}
-	virtual void visit(PlusNode& node) {}
-	virtual void visit(ReturningNode& node) {}
-	virtual void visit(StringLiteral& node) {}
-	virtual void visit(StructDefiningNode& node) {}
-	virtual void visit(StructReferencingNode& node) {}
-	virtual void visit(TypeSpecifier& node) {}
-	virtual void visit(TypeSpecifierByName& node) {}
-	virtual void visit(TypeSpecifierByStruct& node) {}
-	virtual void visit(TypeSpecifierWithTypeDecl& node) {}
-	virtual void visit(WhileLoopingNode& node) {}
 
 protected:
 	SymbolTable& currentSymbolTable() { return *symtab_; }
