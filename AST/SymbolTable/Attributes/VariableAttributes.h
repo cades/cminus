0e703a638a755b3badbb002c603ede10da5f27b4
hong-jen kao <endless-dreaming@hotmail.com>
2012-06-23 17:42:54 +0800

    修正bug: VariableAttributes 實作兩個虛擬函式 剛剛竟然忘記編譯就 commit 了...大忌！

--- AST/SymbolTable/Attributes/VariableAttributes.h
+++ AST/SymbolTable/Attributes/VariableAttributes.h
@@ -9,11 +9,16 @@
 #define VARIABLEATTRIBUTES_H_
 
 #include "Attributes.h"
+#include "../TypeDescriptor/TypeDescriptor.h"
 
 class VariableAttributes: public Attributes {
 public:
 	VariableAttributes();
 	virtual ~VariableAttributes();
+	virtual TypeDescriptor* getType() const { return typeDesc_; }
+	virtual void setType(TypeDescriptor* td) { typeDesc_ = td; }
+private:
+	TypeDescriptor* typeDesc_;
 };
 
 #endif /* VARIABLEATTRIBUTES_H_ */
