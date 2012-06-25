64fc2fa50f39f08c171da5a74d35ed6f310b8c93
hong-jen kao <endless-dreaming@hotmail.com>
2012-06-26 00:31:01 +0800

    在 Visitor 中補完遺漏的受訪 Node 受影響的有 1. 抽象類別 NodeVisitor 3. 型別檢驗第一層的 SemanticsVisitor

--- AST/Visitor/DeclarationProcessing/SemanticsVisitor.h
+++ AST/Visitor/DeclarationProcessing/SemanticsVisitor.h
@@ -16,26 +16,32 @@ public:
 	virtual ~SemanticsVisitor();
 
 	virtual void visit(ANDNode& node) {}
-	virtual void visit(ArrayDefiningNode&) {}
+	virtual void visit(ArrayDefiningNode& node) {}
 	virtual void visit(ArrayReferencingNode& node) {}
 	virtual void visit(ArrayVariableDeclaringNode& node) {}
 	virtual void visit(AssigningNode& node) {}
+	virtual void visit(BinaryExpression&) {}
 	virtual void visit(BlockNode& node) {}
 	virtual void visit(CallingNode& node) {}
 	virtual void visit(DivideNode& node) {}
 	virtual void visit(EQNode& node) {}
 	virtual void visit(EmptyNode& node) {}
+	virtual void visit(Expression&) {}
+	virtual void visit(Factor&) {}
 	virtual void visit(FloatLiteral& node) {}
 	virtual void visit(ForLoopingNode& node) {}
 	virtual void visit(FunctionDeclaringNode& node) {}
 	virtual void visit(GENode& node) {}
 	virtual void visit(GTNode& node) {}
-	virtual void visit(Identifier&) {}
+	virtual void visit(Identifier& node) {}
+	virtual void visit(IdentifierList&) {}
 	virtual void visit(IdentifierWithDim& node) {}
+	virtual void visit(IdentifierWithInitExpr&) {}
 	virtual void visit(IfTestingNode& node) {}
 	virtual void visit(IntLiteral& node) {}
 	virtual void visit(LENode& node) {}
 	virtual void visit(LTNode& node) {}
+	virtual void visit(Literal&) {}
 	virtual void visit(MinusNode& node) {}
 	virtual void visit(MultiplyNode& node) {}
 	virtual void visit(NENode& node) {}
@@ -45,8 +51,8 @@ public:
 	virtual void visit(ORNode& node) {}
 	virtual void visit(PlusNode& node) {}
 	virtual void visit(ReturningNode& node) {}
-	virtual void visit(StructDefiningNode&) {}
 	virtual void visit(StringLiteral& node) {}
+	virtual void visit(StructDefiningNode& node) {}
 	virtual void visit(StructReferencingNode& node) {}
 	virtual void visit(TypeDeclaringNode& node) {}
 	virtual void visit(TypeSpecifier& node) {}
