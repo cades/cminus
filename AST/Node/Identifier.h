6ea1ade88ffab7664184a584c5c0581d7b361f99
hong-jen kao <endless-dreaming@hotmail.com>
2012-06-28 00:04:52 +0800

    修正 Bug: Identifier 含有與 Expression 一樣的函式造成遮蔽 在讓 Identifier 改為繼承 Expression 時沒有發現的 Bug。

--- AST/Node/Identifier.h
+++ AST/Node/Identifier.h
@@ -25,12 +25,9 @@ public:
 	string name() const { return name_; }
 	Attributes* getAttributes() const { return attributesRef_; }
 	void setAttributes(Attributes* attr);
-	TypeDescriptor *getType() const { return type_; }
-	void setType(TypeDescriptor *type_) { this->type_ = type_; }
 private:
 	string name_;
 	Attributes* attributesRef_;
-	TypeDescriptor* type_;
 };
 
 #endif /* IDENTIFIER_H_ */
