274d40c6920c8f9774fbb52a9442d72e309938d6
hong-jen kao <endless-dreaming@hotmail.com>
2012-06-27 23:27:39 +0800

    LHSSemanticsVisitor 完成 LHSSemanticsVisitor 只處理三種 Node: Identifier, ArrayReferencingNode, StructReferencingNode. 另設輔助函式 isAssignable, 在 isDataObject 會通過的 function call 在這裡會被過濾掉.

--- AST/Visitor/DeclarationProcessing/LHSSemanticsVisitor.cpp
+++ AST/Visitor/DeclarationProcessing/LHSSemanticsVisitor.cpp
@@ -6,6 +6,15 @@
  */
 
 #include "LHSSemanticsVisitor.h"
+#include <ostream>
+#include "../../Node/Identifier.h"
+#include "../../Node/ArrayReferencingNode.h"
+#include "../../Node/StructReferencingNode.h"
+
+#include "../../SymbolTable/Attributes/Attributes.h"
+#include "../../SymbolTable/Attributes/VariableAttributes.h"
+#include "../../SymbolTable/Attributes/FieldAttributes.h"
+#include "../../SymbolTable/TypeDescriptor/StructTypeDescriptor.h"
 
 LHSSemanticsVisitor::LHSSemanticsVisitor(SymbolTable* symtab, ostream& os) : SemanticsVisitor(symtab, os) {
 	// TODO Auto-generated constructor stub
@@ -15,3 +24,41 @@ LHSSemanticsVisitor::LHSSemanticsVisitor(SymbolTable* symtab, ostream& os) : Sem
 LHSSemanticsVisitor::~LHSSemanticsVisitor() {
 	// TODO Auto-generated destructor stub
 }
+
+void LHSSemanticsVisitor::visit(Identifier& id) {
+	SemanticsVisitor visitor(&currentSymbolTable(), errorLog());
+	id.accept(visitor);
+	if (!isAssignable(id.getAttributes())) {
+		errorLog() << id.name() << " is not assignable.\n";
+		id.setAttributes(0); // NOTICE by design, null attribute and error type is 0.
+	}
+}
+
+void LHSSemanticsVisitor::visit(ArrayReferencingNode & ar) {
+	ar.getVarRef()->accept(*this);
+	SemanticsVisitor visitor(&currentSymbolTable(), errorLog());
+	ar.accept(visitor);
+}
+
+void LHSSemanticsVisitor::visit(StructReferencingNode & sr) {
+	SemanticsVisitor visitor(&currentSymbolTable(), errorLog());
+	sr.accept(visitor);
+	if (sr.getType() != 0) { // NOTICE using errorType
+		sr.getVarRef()->accept(*this);
+		if (StructTypeDescriptor* structTypeDesc = dynamic_cast<StructTypeDescriptor*>(sr.getVarRef()->getType())) {
+			Attributes* attributeRef = structTypeDesc->getSymbolTable().retrieveSymbol(sr.getField()->name());
+			if (!isAssignable(attributeRef))
+				errorLog() << sr.getField()->name() << " is not an assignable field.\n";
+		}
+	}
+}
+
+bool LHSSemanticsVisitor::isAssignable(Attributes *attr) {
+	if (dynamic_cast<VariableAttributes*>(attr) ||
+			dynamic_cast<FieldAttributes*>(attr) )
+		return true;
+	return false;
+}
+
+
+
--- AST/Visitor/DeclarationProcessing/LHSSemanticsVisitor.h
+++ AST/Visitor/DeclarationProcessing/LHSSemanticsVisitor.h
@@ -9,11 +9,17 @@
 #define LHSSEMANTICSVISITOR_H_
 
 #include "SemanticsVisitor.h"
+class Attributes;
 
 class LHSSemanticsVisitor: public SemanticsVisitor {
 public:
 	LHSSemanticsVisitor(SymbolTable*, ostream&);
 	virtual ~LHSSemanticsVisitor();
+
+	virtual void visit(Identifier&);
+	virtual void visit(ArrayReferencingNode&);
+	virtual void visit(StructReferencingNode&);
+	bool isAssignable(Attributes*);
 };
 
 #endif /* LHSSEMANTICSVISITOR_H_ */
