9c784832afd5ccb1ed17cce4777253ee9bae0715
hong-jen kao <endless-dreaming@hotmail.com>
2012-06-27 14:57:47 +0800

    新增 CallingNode 與 ArrayReferencingNode 的 Iterator

--- AST/Node/ArrayReferencingNode.h
+++ AST/Node/ArrayReferencingNode.h
@@ -18,6 +18,28 @@ public:
 	virtual ~ArrayReferencingNode();
 	virtual Literal* evaluate() { throw std::runtime_error("ArrayReferencingNode not yet support evaluate()"); } // TODO implement it
 	virtual void accept(NodeVisitor& visitor);
+
+    Expression *getDimExpr() const{ return dimExpr_; }
+    Expression *getVarRef() const { return varRef_; }
+
+    // Composite method API #2
+	class Iterator : public IIterator {
+	public:
+		Iterator(ArrayReferencingNode& ar) : ar_(ar), cnt_(0) {}
+		virtual void First() { cnt_ = 0; }
+		virtual void Next()  { ++cnt_; }
+		virtual bool IsDone() const { return cnt_ == 2; }
+		virtual Expression* CurrentItem() const {
+			if (cnt_ == 0) return ar_.varRef_;
+			else if (cnt_ == 1) return ar_.dimExpr_;
+			else throw std::runtime_error("ArrayReferencingNode::Iterator error");
+		}
+	private:
+		ArrayReferencingNode& ar_;
+		int cnt_;
+	};
+	virtual Iterator* createIterator() { return new Iterator(*this); }
+
 private:
 	Expression* varRef_;
 	Expression* dimExpr_;
--- AST/Node/CallingNode.h
+++ AST/Node/CallingNode.h
@@ -13,6 +13,8 @@ class Identifier;
 class NodeList;
 class NodeVisitor;
 #include <stdexcept>
+#include "NodeList.h"
+#include "Identifier.h"
 
 class CallingNode: public Expression {
 public:
@@ -20,6 +22,28 @@ public:
 	virtual ~CallingNode();
 	virtual void accept(NodeVisitor& visitor);
 	virtual Literal* evaluate() { throw std::runtime_error("CallingNode DONOT support evaluate()."); }
+
+	// Composite method API #2
+	class Iterator : public IIterator {
+	public:
+		Iterator(CallingNode& cn) : cn_(cn), cnt_(0) { i_ = cn_.argList_->createIterator(); }
+		virtual void First() { cnt_ = 0; i_->First(); }
+		virtual void Next()  {
+			++cnt_;
+			if (cnt_ > 0) i_->Next();
+		}
+		virtual bool IsDone() const { return cnt_ == 1 + cn_.argList_->length(); }
+		virtual AbstractNode* CurrentItem() const {
+			if (cnt_ == 0) return cn_.functionName_;
+			else return i_->CurrentItem();
+		}
+	private:
+		CallingNode& cn_;
+		int cnt_;
+		NodeList::Iterator* i_;
+	};
+	virtual Iterator* createIterator() { return new Iterator(*this); }
+
 private:
 	Identifier* functionName_;
 	NodeList* argList_;
