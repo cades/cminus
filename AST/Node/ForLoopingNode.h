7c36c6ded6d4d815d32c168e2f1ed2c5aa3a41c0
hong-jen kao <endless-dreaming@hotmail.com>
2012-06-28 11:52:13 +0800

    if, while, for 三種node新增 Iterator 介面

--- AST/Node/ForLoopingNode.h
+++ AST/Node/ForLoopingNode.h
@@ -9,7 +9,8 @@
 #define FORLOOPINGNODE_H_
 
 #include "AbstractNode.h"
-class ExpressionList;
+#include <stdexcept>
+#include "ExpressionList.h"
 class NodeVisitor;
 
 class ForLoopingNode: public AbstractNode {
@@ -17,6 +18,27 @@ public:
 	ForLoopingNode(ExpressionList* initializer, ExpressionList* condition, ExpressionList* increment,  AbstractNode* loopBody);
 	virtual ~ForLoopingNode();
 	virtual void accept(NodeVisitor& visitor);
+
+	// Composite method API #2
+	class Iterator : public IIterator {
+	public:
+		Iterator(ForLoopingNode& node) : node_(node), cnt_(0) {}
+		virtual void First() { cnt_ = 0; }
+		virtual void Next()  { ++cnt_; }
+		virtual bool IsDone() const { return cnt_ == 4; }
+		virtual AbstractNode* CurrentItem() const {
+			if (cnt_ == 0) return node_.initializer_;
+			else if (cnt_ == 1) return node_.condition_;
+			else if (cnt_ == 2) return node_.increment_;
+			else if (cnt_ == 3) return node_.loopBody_;
+			else throw std::runtime_error("ForLoopingNode::Iterator error");
+		}
+	private:
+		ForLoopingNode& node_;
+		int cnt_;
+	};
+	virtual Iterator* createIterator() { return new Iterator(*this); }
+
 private:
 	ExpressionList* initializer_;
 	ExpressionList* condition_;
--- AST/Node/WhileLoopingNode.h
+++ AST/Node/WhileLoopingNode.h
@@ -9,7 +9,8 @@
 #define WHILELOOPINGNODE_H_
 
 #include "AbstractNode.h"
-class ExpressionList;
+#include <stdexcept>
+#include "ExpressionList.h"
 class NodeVisitor;
 
 class WhileLoopingNode: public AbstractNode {
@@ -17,6 +18,25 @@ public:
 	WhileLoopingNode(ExpressionList* condition,  AbstractNode* loopBody);
 	virtual ~WhileLoopingNode();
 	virtual void accept(NodeVisitor& visitor);
+
+	// Composite method API #2
+	class Iterator : public IIterator {
+	public:
+		Iterator(WhileLoopingNode& node) : node_(node), cnt_(0) {}
+		virtual void First() { cnt_ = 0; }
+		virtual void Next()  { ++cnt_; }
+		virtual bool IsDone() const { return cnt_ == 2; }
+		virtual AbstractNode* CurrentItem() const {
+			if (cnt_ == 0) return node_.condition_;
+			else if (cnt_ == 1) return node_.loopBody_;
+			else throw std::runtime_error("WhileLoopingNode::Iterator error");
+		}
+	private:
+		WhileLoopingNode& node_;
+		int cnt_;
+	};
+	virtual Iterator* createIterator() { return new Iterator(*this); }
+
 private:
 	ExpressionList* condition_;
 	AbstractNode* loopBody_;
